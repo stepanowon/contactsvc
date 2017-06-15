ca73c32a1f544df2d1701518a066c128bd47bb4f
Hyungseob Won <stepanowon@hotmail.com>
2017-06-16 00:37:50 +0900

    Create server.js

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 //var cors = require('cors');
-var path = require('path');
-var fs = require('fs');
+//var morgan = require('morgan');
+//var path = require('path');
+//var fs = require('fs');
+//var rfs = require('rotating-file-stream');
 
 //app.use(cors());
 app.enable("jsonp callback");   //jsonp 지원
+//-- 로깅
+/**
+var logDirectory = path.join(__dirname, 'log')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // 매일 매일 로그 파일 생성
+  path: logDirectory
+})
+app.use(morgan('combined', {stream: accessLogStream}))
+*/
 
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
