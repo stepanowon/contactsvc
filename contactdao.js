672acc158017df5e4ec34f083f37f4f4b1c7a074
Hyungseob Won <stepanowon@hotmail.com>
2017-06-20 20:08:47 +0900

    Update contactdao.js

--- contactdao.js
+++ contactdao.js
@@ -16,11 +16,12 @@ var initializeDB = function() {
                 db.run("CREATE TABLE contacts (no INTEGER PRIMARY KEY, name TEXT, tel TEXT, address TEXT, photo TEXT)");
                 var stmt = db.prepare("INSERT INTO contacts (no, name,tel,address, photo) VALUES (?,?,?,?,?)");
                 for (var i=0; i < 100; i++) {
-                    no++;
                     var num=""+no;
                     if (no < 10) num = "0"+no;
                     var ridx = Math.floor(Math.random() * arrlNames.length);
-                    stmt.run(base+no, arrfNames[i] + " " + arrlNames[ridx], "010-3456-82"+num, "서울시", "" + no + ".jpg");  
+                    
+                    stmt.run(base+no, arrfNames[i] + " " + arrlNames[ridx], "010-3456-82"+num, "서울시", "" + (no+1) + ".jpg");
+                    no++;
                 }
                 stmt.finalize();
             });
@@ -50,32 +51,40 @@ var  injectionfilter = function(val){
 module.exports = {
     insertContact : function(name,tel,address) {
          return new Promise(function(resolve, reject) {
-             var nextno = (new Date()).getTime();
-             db.run("INSERT INTO contacts VALUES (?,?,?,?,?)",[nextno,name,tel,address,'noimage.jpg'], function(err) {
-                 if (err) {
-                     reject({ status:'fail', message:error });
-                 } else {
-                     resolve({ status:'success', message:'No(' + this.lastID + ') 데이터 추가 성공!!', no:this.lastID });
-                 }
-             })
+             if (name && tel && name!="" && tel!="") {
+                var nextno = (new Date()).getTime();
+                db.run("INSERT INTO contacts VALUES (?,?,?,?,?)",[nextno,name,tel,address,'noimage.jpg'], function(err) {
+                    if (err) {
+                        resolve({ status:'fail', message:'연락처 추가 실패 : ' +err });
+                    } else {
+                        resolve({ status:'success', message:'No(' + this.lastID + ') 데이터 추가 성공!!', no:this.lastID });
+                    }
+                })
+             } else {
+                resolve({ status:'fail', message:'이름과 전화번호는 반드시 입력해야 합니다.' });
+             }
          });
     },
     updateContact : function(no,name,tel,address) {
          return new Promise(function(resolve, reject) {
-             db.run("UPDATE contacts SET name=?, tel=?, address=? WHERE no=?", [name,tel,address,no], function(err) {
-                 if (err) {
-                     reject({ status:'fail', message:error });
-                 } else {
-                     resolve({ status:'success', message:'No(' + no + ') 데이터 변경 성공!!', no:no });
-                 }
-             })
+             if (no && name && tel && name!="" && tel!="") {
+                db.run("UPDATE contacts SET name=?, tel=?, address=? WHERE no=?", [name,tel,address,no], function(err) {
+                    if (err) {
+                        resolve({ status:'fail', message:'연락처 변경 실패 : ' + err });
+                    } else {
+                        resolve({ status:'success', message:'No(' + no + ') 데이터 변경 성공!!', no:no });
+                    }
+                })
+             } else {
+                 resolve({ status:'fail', message:'일련번호, 이름, 전화번호는 반드시 입력해야 합니다.' });
+             }
          });
     },
     deleteContact : function(no) {
          return new Promise(function(resolve, reject) {
              db.run("DELETE FROM contacts WHERE no=?", no, function(err) {
                  if (err) {
-                     reject({ status:'fail', message:error });
+                     resolve({ status:'fail', message:'삭제 실패 : ' +err });
                  } else {
                      resolve({ status:'success', message:'No(' + no + ') 데이터 삭제 성공!!', no:no });
                  }
@@ -139,10 +148,10 @@ module.exports = {
         return new Promise(function(resolve, reject) {
             var injResult = injectionfilter(name);
             if (injResult == true) {
-                reject({ message:"이름에 인젝션 문자열을 포함하고 있으므로 실행하지 않습니다."});
+                resolve({ status:'fail', message:"이름에 인젝션 문자열을 포함하고 있으므로 실행하지 않습니다."});
             }
             if (name.length < 2) {
-                reject({ message:"2글자 이상만 검색이 가능합니다."});
+                resolve({ status:'fail', message:"2글자 이상만 검색이 가능합니다."});
             }
             var sql = "SELECT no,name,tel,address,photo FROM contacts WHERE name LIKE '%" + name + "%' ORDER BY name ASC"
             db.all(sql, function(err, rows) {
@@ -172,7 +181,7 @@ module.exports = {
                     stmt.finalize();
                     resolve({ status:'success', message: i+'건 데이터 추가 성공!!', no : insertedRows });
                 } catch(e) {
-                    reject({ status:'fail', message:e });
+                    resolve({ status:'fail', message:'배치 추가 작업 실패 : ' + e });
                 }
             });
         });
@@ -181,15 +190,15 @@ module.exports = {
          return new Promise(function(resolve, reject) {
              db.get("SELECT no FROM contacts WHERE no=?", no, function(err, row) {
                  if (row) {
-                    db.run("UPDATE contacts SET photo=? WHERE no=?", [photo,no], function(err) {
+                    db.run("UPDATE contacts SET photo=? WHERE no=?", [photo, no], function(err) {
                         if (err) {
-                            reject({ status:'fail', message:error });
+                            resolve({ status:'fail', message:'사진 변경 실패 : '+err });
                         } else {
                             resolve({ status:'success', message:'No(' + no + ') 사진 변경 성공!!', no:no });
                         }
                     })
                  } else {
-                     reject({ status:'fail', message:"no에 해당하는 연락처 정보 없음" })
+                     resolve({ status:'fail', message:"no에 해당하는 연락처 정보 없음" })
                  }
              })
 
